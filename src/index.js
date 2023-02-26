53b0ecd9cb0c1195529a7028764b0d1963a4a14d
Mattews12 <gargi.hockey12@gmail.com>
2023-02-27 02:13:18 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-    
+    return Math.abs(n).toString().split("").reverse().join("");
+    // Math.abs() - возвращаем абсолютное число
+    // toString() - делаем строку
+    // split() - разбиваем строку на элементы массива
+    // reverse() - переворачиваем массив 
+    // join() - cобираем все элементы обратно в строку
 };
